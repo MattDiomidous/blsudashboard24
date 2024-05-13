14495d0d92ad0db6e6fec9cf02173451f6b3b7a9
zeypen24 <71848482+zeypen24@users.noreply.github.com>
2024-05-13 09:45:02 -0400

    Revert "Fixing Frontend"
    This reverts commit bb217faac85deb6b01433be7574b000ca6a57ddd.

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const path = require('path');
 const mysql = require('mysql2/promise');
 const bodyParser = require('body-parser');
 
-
 const config = {
   authRequired: false,
   auth0Logout: true,
@@ -35,9 +34,6 @@ app.use(auth(config));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.json());
-
-
-
 
 app.use(async (req, res, next) => {
   if (req.oidc.isAuthenticated()) {
